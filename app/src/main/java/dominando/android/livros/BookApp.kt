18cbb44cfb99af4094bf98c8a9d127267d04413d
Gabriel Brasileiro <gah.lucas@gmail.com>
2020-10-17 03:06:20 -0300

    Addition of module to application

--- app/src/main/java/dominando/android/livros/BookApp.kt
+++ app/src/main/java/dominando/android/livros/BookApp.kt
@@ -2,6 +2,7 @@ package dominando.android.livros
 
 import android.app.Application
 import dominando.android.data.di.DataModules
+import dominando.android.data_fb.di.DataFBModules
 import dominando.android.data_room.di.DataRoomModules
 import dominando.android.domain.di.DomainModules
 import dominando.android.livros.di.AppModule
@@ -13,6 +14,7 @@ class BookApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
         startKoin {
             androidContext(this@BookApp)
         }
@@ -23,6 +25,7 @@ class BookApp : Application() {
     private fun loadAllModules() {
         AppModule.load()
         DataRoomModules.load()
+        DataFBModules.load()
         DataModules.load()
         DomainModules.load()
         PresentationModule.load()
